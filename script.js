65b08366d824b75c022d74374208e5885c8426de
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-12-11 23:33:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function getData(){
+	const request = new XMLHttpRequest();
+         request.open('GET',"https://jsonplaceholder.typicode.com/todos/1");
+         request.send();
+        
+        // to get the response that is sent
+        request.addEventListener('load',function(){
+         const data = JSON.parse(this.responseText);
+			console.log(data)
+		})
+}
+getData();
